6a1e48e11ef6131a2117251e4774a84a0dbc4dd6
Kateryna Lukianova <kettibellya@gmail.com>
2018-09-09 20:36:03 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.calc .digits-oper')
+    .forEach( el => el.addEventListener('click', pressedDigitOrOper));
+    
+    
+function pressedDigitOrOper(ev) {
+    const displ = document.querySelector('.calc .displ');
+    displ.value += ev.target.innerText;
+}
+
+document.querySelector('.calc .eval')
+    .addEventListener('click', evaluate);
+
+function evaluate() {
+    const displ = document.querySelector('.calc .displ');
+    displ.value = eval(displ.value);
+}
+
+document.querySelector('.calc .clear')
+    .addEventListener('click', clearAll);
+    
+function clearAll() {
+    const displ = document.querySelector('.calc .displ');
+    displ.value = "";
+}
+
+document.querySelector('.calc .sqrt')
+    .addEventListener('click', mathSqrtOur);
+    
+function mathSqrtOur () {
+    const displ = document.querySelector('.calc .displ');
+    displ.value = Math.sqrt(displ.value);
+}
+        
